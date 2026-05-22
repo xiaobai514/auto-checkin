87b263e1c3f5496a99b168a715d5a380d5adb34d
xiaobai514 <q748949062@gmail.com>
2026-05-22 12:23:44 +0800

    fix: 使用 SMTP_SSL 连接 QQ 邮箱 (465端口)

--- checkin.py
+++ checkin.py
@@ -82,8 +82,7 @@ def send_email(subject, body):
         """
         msg.attach(MIMEText(html_body, "html", "utf-8"))
         
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
+        with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as server:
             server.login(SMTP_USER, SMTP_PASS)
             server.send_message(msg)
         
